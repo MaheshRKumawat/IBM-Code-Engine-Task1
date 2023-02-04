2640bdc771e12611251ad852d37edf25c3b2786b
MaheshRKumawat <maheshkumawat0911@gmail.com>
2023-02-05 00:44:27 +0530

    Made some changes

--- preprocessing/main.go
+++ preprocessing/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"bytes"
 	"encoding/csv"
+	"encoding/json"
 	"fmt"
 	"io"
 	"io/ioutil"
@@ -15,9 +17,9 @@ import (
 	"github.com/IBM/ibm-cos-sdk-go/service/s3"
 )
 
-func preprocess(fileName string) (rec string) {
+func preprocess(fileName string) {
 	fi, erri := os.Open(fileName)
-	fo, erro := os.Create("salesP.csv")
+	fo, erro := os.Create("Preprocessed.csv")
 
 	if erri != nil {
 		fmt.Printf("Failed opening file, error: %s", erri)
@@ -56,8 +58,6 @@ func preprocess(fileName string) (rec string) {
 
 	fi.Close()
 	fo.Close()
-
-	return rec
 }
 
 func main() {
@@ -65,7 +65,9 @@ func main() {
 	serviceInstanceID := os.Getenv("RESOURCE_INSTANCE_ID")
 	authEndpoint := os.Getenv("AUTH_ENDPOINT")
 	serviceEndpoint := os.Getenv("SERVICE_ENDPOINT")
-	// bucketLocation := os.Getenv("LOCATION")                                   
+	bucketName := os.Getenv("BUCKET_NAME")
+	prevkey := os.Getenv("DATASET_OBJECT_KEY")
+	currkey := os.Getenv("PREPROCESSED_OBJECT_KEY")
 
 	// Create config
 	conf := aws.NewConfig().
@@ -74,24 +76,55 @@ func main() {
 		WithCredentials(ibmiam.NewStaticCredentials(aws.NewConfig(), authEndpoint, apiKey, serviceInstanceID)).
 		WithS3ForcePathStyle(true)
 
-	// Create client
 	sess := session.Must(session.NewSession())
 	client := s3.New(sess, conf)
 
-	_, err := client.ListBuckets(&s3.ListBucketsInput{})
+	list_objects := &s3.ListObjectsV2Input{
+		Bucket: aws.String(bucketName),
+	}
 
-	// fmt.Print(d)
+	l, _ := client.ListObjectsV2(list_objects)
 
-	fmt.Println("error: ", err)
+	type ob []map[string]string
+	var jsonMap map[string]ob
+	var ob_keys []string
 
-	// Variables
-	bucketName := os.Getenv("BUCKET_NAME")
-	key := os.Getenv("DATASET_OBJECT_KEY")
+	jsonBytes, _ := json.MarshalIndent(l, " ", " ")
+	json.Unmarshal(jsonBytes, &jsonMap)
+	objects := jsonMap["Contents"]
+
+	for _, v := range objects {
+		ob_keys = append(ob_keys, v["Key"])
+	}
+
+	prevObjectPresent := false
+	currObjectPresent := false
+
+	for _, obj := range ob_keys {
+		if obj == prevkey {
+			prevObjectPresent = true
+		}
+		if obj == currkey {
+			currObjectPresent = true
+		}
+	}
+
+	if !prevObjectPresent {
+		log.Fatalln("Dataset Object not present in Cloud Object Storage")
+		log.Fatalln("Exit from main.go")
+		os.Exit(1)
+	}
+
+	if currObjectPresent {
+		log.Fatalln("Preprocessed Object already present in Cloud Object Storage")
+		log.Fatalln("Exit from main.go")
+		os.Exit(1)
+	}
 
 	// users will need to create bucket, key (flat string name)
 	Input := s3.GetObjectInput{
 		Bucket: aws.String(bucketName),
-		Key:    aws.String(key),
+		Key:    aws.String(prevkey),
 	}
 
 	// Call Function
@@ -99,21 +132,44 @@ func main() {
 
 	body, _ := ioutil.ReadAll(res.Body)
 
-	preprocess(string(body))
+	data := string(body)
 
-	// fmt.Print(body)
+	csvFile, err := os.Create(prevkey)
 
-	// // Variables and random content to sample, replace when appropriate
-	// Newkey := os.Getenv("PREPROCESSED_OBJECT_KEY")
-	// content := bytes.NewReader([]byte(contentfile))
+	if err != nil {
+		log.Fatalln("Failed to create file: ", err)
+		log.Fatalln("Exit from main.go")
+		os.Exit(1)
+	}
 
-	// input := s3.PutObjectInput{
-	// 	Bucket: aws.String(bucketName),
-	// 	Key:    aws.String(Newkey),
-	// 	Body:   content,
-	// }
+	_, err = csvFile.WriteString(data)
 
-	// // Call Function to upload (Put) an object
-	// result, _ := client.PutObject(&input)
-	// fmt.Println(result)
+	if err != nil {
+		log.Fatalln("Failed to write file: ", err)
+		log.Fatalln("Exit from main.go")
+		os.Exit(1)
+	}
+
+	preprocess(prevkey)
+
+	DataBytes, erri := ioutil.ReadFile(currkey)
+
+	if erri != nil {
+		fmt.Printf("Failed opening file, error: %s", erri)
+		os.Exit(1)
+	}
+
+	content := bytes.NewReader([]byte(DataBytes))
+
+	input := s3.PutObjectInput{
+		Bucket: aws.String(bucketName),
+		Key:    aws.String(currkey),
+		Body:   content,
+	}
+
+	// Call Function to upload (Put) an object
+	result, _ := client.PutObject(&input)
+	if result != nil {
+		fmt.Println("Preprocessed Object pushed to Cloud Object Storage")
+	}
 }
