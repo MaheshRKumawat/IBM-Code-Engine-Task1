9e5b59096a6dfa20bcdd75d6697a3a814ac48e4e
MaheshRKumawat <maheshkumawat0911@gmail.com>
2023-02-05 00:48:04 +0530

    Bug Fixes

--- update_price/main.go
+++ update_price/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bytes"
 	"encoding/csv"
+	"encoding/json"
 	"fmt"
 	"io"
 	"io/ioutil"
@@ -58,7 +59,7 @@ func updatePrice(priceFile string) {
 		count++
 	}
 
-	fo, erro := os.Create("UpdatedPrice.csv")
+	fo, erro := os.Create("Final_Output.csv")
 	csvWriter := csv.NewWriter(fo)
 	if erro != nil {
 		log.Fatalf("Failed creating file, error: %s", erro)
@@ -77,7 +78,9 @@ func main() {
 	serviceInstanceID := os.Getenv("RESOURCE_INSTANCE_ID")
 	authEndpoint := os.Getenv("AUTH_ENDPOINT")
 	serviceEndpoint := os.Getenv("SERVICE_ENDPOINT")
-	// bucketLocation := os.Getenv("LOCATION")
+	bucketName := os.Getenv("BUCKET_NAME")
+	prevkey := os.Getenv("TOP_PRODUCTS_OBJECT_KEY")
+	currkey := os.Getenv("FINAL_OUTPUT_OBJECT_KEY")
 
 	// Create config
 	conf := aws.NewConfig().
@@ -86,24 +89,55 @@ func main() {
 		WithCredentials(ibmiam.NewStaticCredentials(aws.NewConfig(), authEndpoint, apiKey, serviceInstanceID)).
 		WithS3ForcePathStyle(true)
 
-	// Create client
 	sess := session.Must(session.NewSession())
 	client := s3.New(sess, conf)
 
-	d, err := client.ListBuckets(&s3.ListBucketsInput{})
+	list_objects := &s3.ListObjectsV2Input{
+		Bucket: aws.String(bucketName),
+	}
 
-	fmt.Print(d)
+	l, _ := client.ListObjectsV2(list_objects)
 
-	fmt.Println("error: ", err)
+	type ob []map[string]string
+	var jsonMap map[string]ob
+	var ob_keys []string
 
-	// Variables
-	bucketName := os.Getenv("BUCKET_NAME")
-	key := os.Getenv("REDUCER_OBJECT_KEY")
+	jsonBytes, _ := json.MarshalIndent(l, " ", " ")
+	json.Unmarshal(jsonBytes, &jsonMap)
+	objects := jsonMap["Contents"]
+
+	for _, v := range objects {
+		ob_keys = append(ob_keys, v["Key"])
+	}
+
+	prevObjectPresent := false
+	currObjectPresent := false
+
+	for _, obj := range ob_keys {
+		if obj == prevkey {
+			prevObjectPresent = true
+		}
+		if obj == currkey {
+			currObjectPresent = true
+		}
+	}
+
+	if !prevObjectPresent {
+		log.Fatalln("Top Products Price Object not present in Cloud Object Storage")
+		log.Fatalln("Exit from main.go")
+		os.Exit(1)
+	}
+
+	if currObjectPresent {
+		log.Fatalln("Final Output Object already present in Cloud Object Storage")
+		log.Fatalln("Exit from main.go")
+		os.Exit(1)
+	}
 
 	// users will need to create bucket, key (flat string name)
 	Input := s3.GetObjectInput{
 		Bucket: aws.String(bucketName),
-		Key:    aws.String(key),
+		Key:    aws.String(prevkey),
 	}
 
 	// Call Function
@@ -111,19 +145,44 @@ func main() {
 
 	body, _ := ioutil.ReadAll(res.Body)
 
-	updatePrice(string(body))
+	data := string(body)
 
-	// Variables and random content to sample, replace when appropriate
-	Newkey := os.Getenv("FINAL_OBJECT_KEY")
-	content := bytes.NewReader([]byte("<CONTENT>"))
+	csvFile, err := os.Create(prevkey)
+
+	if err != nil {
+		log.Fatalln("Failed to create file: ", err)
+		log.Fatalln("Exit from main.go")
+		os.Exit(1)
+	}
+
+	_, err = csvFile.WriteString(data)
+
+	if err != nil {
+		log.Fatalln("Failed to write file: ", err)
+		log.Fatalln("Exit from main.go")
+		os.Exit(1)
+	}
+
+	updatePrice(prevkey)
+
+	DataBytes, erri := ioutil.ReadFile(currkey)
+
+	if erri != nil {
+		fmt.Printf("Failed opening file, error: %s", erri)
+		os.Exit(1)
+	}
+
+	content := bytes.NewReader([]byte(DataBytes))
 
 	input := s3.PutObjectInput{
 		Bucket: aws.String(bucketName),
-		Key:    aws.String(Newkey),
+		Key:    aws.String(currkey),
 		Body:   content,
 	}
 
 	// Call Function to upload (Put) an object
 	result, _ := client.PutObject(&input)
-	fmt.Println(result)
+	if result != nil {
+		fmt.Println("Final Object pushed to Cloud Object Storage")
+	}
 }
